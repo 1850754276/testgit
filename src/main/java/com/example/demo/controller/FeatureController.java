d8f268c9f197776c93e89666d5a4968cabe5a6f6
lzhiwei <1850754276@qq.com>
2021-05-11 11:34:10 +0800

    feature-20210511-i18n

---
+++ src/main/java/com/example/demo/controller/FeatureController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("feature/")
+public class FeatureController {
+
+    @GetMapping("one")
+    public String feature() {
+        return "feature one";
+    }
+}
